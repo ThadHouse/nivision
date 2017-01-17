e337b8e24cca3a730f90ca0b220f67fbdda5ec09
Thad House <thadhouse1@gmail.com>
2017-01-17 14:24:35 -0800

    Actually adds installer

---
+++ buildInstaller.ps1
@@ -0,0 +1,14 @@
+$exePath = "$env:USERPROFILE\innosetup-5.5.9-unicode.exe"
+ 
+Write-Host "Downloading InnoSetup 5.5.9..."
+(New-Object Net.WebClient).DownloadFile('http://files.jrsoftware.org/is/5/innosetup-5.5.9-unicode.exe', $exePath)
+ 
+Write-Host "Installing..."
+cmd /c start /wait $exePath /silent
+ 
+Write-Host "Installed InnoSetup 5.5.9" -ForegroundColor Green
+
+$env:PATH += ";C:\Program Files (x86)\Inno Setup 5"
+$env:PATH += ";C:\Program Files\Inno Setup 5"
+
+.\gradlew installer
